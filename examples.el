26b2ccf09aeb16084368aa03a46b1b36ee80c4c6
Iskander Sharipov <quasilyte@gmail.com>
2018-07-26 00:20:45 +0300

    examples: rename pair fields + other fixes

--- examples.el
+++ examples.el
@@ -1,24 +1,24 @@
 ;; This file constantly changes to reflect the latest vision of the language.
 
 
-;; Define a new type `IntPair' with 2 fields: `x' and `y'. Both fields have a type of `Int'.
+;; Define a new type `IntPair' with 2 fields: `first' and `second'. Both fields have a type of `Int'.
 ;; For every field, getter and setter functions are generated.
 ;; These functions are not exported and must be exported explicitly.
-(type IntPair a Int b Int)
+(type IntPair Int first Int second)
 
 (do ;; Like `progn' in Emacs Lisp, block of forms.
   ;; `var' inside a `do' defines a variable local to that block.
-  (var p1 (new IntPair :a 0 :b 1)) ;; Creates [0 1]
-  (var p2 (new IntPair :b 1))      ;; Same as above, `a' gets zero value
+  (var p1 (new IntPair :first 0 :second 1)) ;; Creates [0 1]
+  (var p2 (new IntPair :second 1))          ;; Same as above, `first' gets zero value
   (if [p1 = p2]                    ;; Performs field-by-field shallow comparison
       (print "${p1} ${p2}")        ;; Can use string interpolation for vars
     (panic "impossible"))          ;; `panic` is borrowed from Go
-  ;; Fields are accessed using `get' special form:
-  (p1.a) ;; Calls `IntPair.a' (auto-generated getter)
-  (p1.b) ;; Calls `IntPair.b' (auto-generated getter)
+  ;; Fields are accessed using auto-generated getters and setter functions.
+  (p1.first)  ;; Calls `IntPair.first' 0-argument overloading (auto-generated getter)
+  (p1.second) ;; Calls `IntPair.second' 0-argument overloading (auto-generated getter)
   ;; `set` special form can assign new values to fields:
-  (p1.a (p1.b)) ;; Calls `IntPair.a' 1-argument overloading (auto-generated setter)
-  (p1.b (p1.a)) ;; Calls `IntPair.b' 1-argument overloading (auto-generated setter)
+  (p1.first (p1.second)) ;; Calls `IntPair.first' 1-argument overloading (auto-generated setter)
+  (p1.second (p1.first)) ;; Calls `IntPair.second' 1-argument overloading (auto-generated setter)
   )
 
 ;; Define a `fact' function of type `[(Int) -> Int]'.
